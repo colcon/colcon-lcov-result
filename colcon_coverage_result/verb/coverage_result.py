a3e007bc6a943612a6b3d8887375fbddcb751b24
Juan Pablo Samper <jp.samper@apex.ai>
2018-09-07 16:08:17 -0700

    Add packages arguments

--- colcon_coverage_result/verb/coverage_result.py
+++ colcon_coverage_result/verb/coverage_result.py
@@ -1,11 +1,10 @@
 # Copyright 2018 Apex.AI, Inc.
 # Licensed under the Apache License, Version 2.0
 
-import argparse
-import os
-
 from colcon_core.logging import colcon_logger
 from colcon_core.plugin_system import satisfies_version
+from colcon_core.package_selection import add_arguments \
+    as add_packages_arguments
 from colcon_core.verb import VerbExtensionPoint
 
 logger = colcon_logger.getChild(__name__)
@@ -20,8 +19,8 @@ class CoverageResultVerb(VerbExtensionPoint):
         super().__init__()
         satisfies_version(VerbExtensionPoint.EXTENSION_POINT_VERSION, '^1.0')
 
-    def add_argument(self, *, parser):
-        return
+    def add_arguments(self, *, parser):
+        add_packages_arguments(parser)
 
     def main(self, *, context):
         print("Hello World!")
